244355969a7847f769c64cf9774362ab5deb5c4d
Oleg <evolrn89@gmail.com>
2024-10-14 18:14:19 +0600

    Created component MessageForm

--- src/components/Message/MessageList.tsx
+++ src/components/Message/MessageList.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Message from './Message';
+import React from "react";
+import Message from "./Message";
 
 interface MessageListProps {
   messages: { message: string; author: string; datetime: string }[];
 }
 
 const MessageList: React.FC<MessageListProps> = React.memo(({ messages }) => {
+  console.log(messages);
   return (
     <div>
       {messages.map((msg) => (
